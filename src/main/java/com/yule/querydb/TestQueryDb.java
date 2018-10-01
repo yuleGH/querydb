c57c6d2db37ab847b2a096f9f5b550351701b060
yuleGH <yule.mail@qq.com>
2018-10-01 12:58:25 +0800

    init

---
+++ src/main/java/com/yule/querydb/TestQueryDb.java
@@ -0,0 +1,16 @@
+package com.yule.querydb;
+
+/**
+ * @author yule
+ * @date 2018/9/30 14:44
+ */
+public class TestQueryDb {
+
+    public static void test(){
+        System.out.println("测试。。。");
+    }
+
+    public static void main(String[] args){
+        System.out.println("sdfdf");
+    }
+}
