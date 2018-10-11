c6db450caedf9d31b9361bdf465fc0c8d8b5c2e7
yuleGH <yule.mail@qq.com>
2018-10-11 09:42:18 +0800

    update

--- src/main/java/com/yule/querydb/servlet/QueryDbDispatcherServlet.java
+++ src/main/java/com/yule/querydb/servlet/QueryDbDispatcherServlet.java
@@ -101,7 +101,7 @@ public class QueryDbDispatcherServlet extends HttpServlet {
      */
     private void returnErrorPage(HttpServletResponse response) throws IOException {
         //不走后台了，以免循环重定向
-        response.sendRedirect("/querydb/error.html");
+        response.sendRedirect("/querydb/error/error.html");
     }
 
     /**
@@ -113,7 +113,7 @@ public class QueryDbDispatcherServlet extends HttpServlet {
     private void return404Page(HttpServletResponse response, String path) throws IOException {
         logger.error("404-找不到地址：{}", path);
         //不走后台了，以免循环重定向
-        response.sendRedirect("/querydb/404.html");
+        response.sendRedirect("/querydb/error/404.html");
     }
 
     /**
